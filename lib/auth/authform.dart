8911ccedecde825eca0cf0ac96b47965e9965497
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-19 01:54:13 +0500

    Update authform.dart

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -8,11 +8,25 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.4,
       width: MediaQuery.of(context).size.width * 0.8,
+      child: ListView(
+        children: [
+          Container(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
