7e1bc7ca37a614ea4c95fb0405545a57dfb1086a
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-18 23:53:09 +0500

    Now Calling the Home() class from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Home(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
