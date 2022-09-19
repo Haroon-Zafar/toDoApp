cb743c3c31a3caa0efda938ca8797b4415693dcf
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-19 23:58:30 +0500

    Update authform.dart

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -25,7 +25,8 @@ class _AuthFormState extends State<AuthForm> {
       child: ListView(
         children: [
           Container(
-            padding: const EdgeInsets.only(left: 20, right: 20),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
             child: Form(
               // this key is for validating Form Data
               key: _formKey,
