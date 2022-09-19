1da7f1f71806d16339141d23fbecdfb0100ac48b
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-19 18:53:18 +0500

    Update authform.dart

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -31,7 +31,6 @@ class _AuthFormState extends State<AuthForm> {
                 children: [
                   TextFormField(
                     // this field is for email.
-                    //  key: is used as a hint for the text field.
                     keyboardType: TextInputType.emailAddress,
                     // A key that uses a value of a particular type to identify itself.
                     key: ValueKey('email'),
