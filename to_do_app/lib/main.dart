044491bd3307000ef544a02d189ffa74ec92f0ae
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-01 21:57:36 +0500

    ThemeData( brightness, primaryColor)

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.purple,
+      ),
     );
   }
 }
