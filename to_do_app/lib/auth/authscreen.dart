ef5a4dbd8460f17b348b24e7dd6d5b2cbb9fd281
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-01 23:39:19 +0500

    body: AuthForm() in authscreen

--- to_do_app/lib/auth/authscreen.dart
+++ to_do_app/lib/auth/authscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/auth/authform.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class _AuthScreenState extends State<AuthScreen> {
       appBar: AppBar(
         title: Text("Authentication"),
       ),
+      body: AuthForm(),
     );
   }
 }
