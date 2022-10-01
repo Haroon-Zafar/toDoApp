d2d83dc01692d584698ae0be22f0f7d4441dc8b6
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-01 23:43:19 +0500

    Returning AuthScreen() in main.dart instead of Home()

--- to_do_app/lib/auth/authform.dart
+++ to_do_app/lib/auth/authform.dart
@@ -11,8 +11,10 @@ class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        // we are returning a container here whose height and width is same as the screen.
-
-        );
+      // we are returning a container here whose height and width is same as the screen.
+      // we are using MediaQuery to get the height and width of the screen.
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+    );
   }
 }
--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/auth/authform.dart';
+import 'package:to_do_app/auth/authscreen.dart';
 import 'package:to_do_app/screens/home.dart';
 
 void main() {
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: AuthScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
