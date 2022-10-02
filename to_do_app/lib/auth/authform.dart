517150267a95c7e61befb1493fe3cdc53e98422f
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-02 23:22:31 +0500

    Made necessary changes in TextFormField() for username

--- to_do_app/lib/auth/authform.dart
+++ to_do_app/lib/auth/authform.dart
@@ -47,6 +47,40 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 // here we are going to define textfields and buttons
                 children: [
+                  // if isLoginPage is true, then we will show the login text. If isLoginPage is false, then we will show the signup text
+
+                  // username field
+                  TextFormField(
+                    // because user to define email address in that field.
+                    keyboardType: TextInputType.emailAddress,
+                    key: ValueKey('username'),
+                    // we are using validator to validate the email address.
+                    validator: (value) {
+                      // if the value is null or empty, then return error message.
+                      // if the value length is less than 4, then return error message.
+                      // if the value is not null or empty, then return null.
+                      if (value!.isEmpty || value.length < 4) {
+                        return 'Incorrect Username';
+                      }
+                      // if the value is not null or empty, then return null.
+                      return null;
+                    },
+                    // we are using onSaved to save the email address.
+                    onSaved: (value) {
+                      // we are saving the email address in _email variable.
+                      var _username = value;
+                    },
+
+                    // Adding some decorations.
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: new BorderSide(),
+                      ),
+                      labelText: "Enter Username",
+                    ),
+                  ),
+
                   // why TextFromField ? because we are using Form() widget, so we need to use TextFormField() widget.
                   TextFormField(
                     // because user to define email address in that field.
