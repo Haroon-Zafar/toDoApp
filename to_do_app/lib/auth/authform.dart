9d5dd0238654cffdff641f0d5c46603fa3766558
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-02 23:18:48 +0500

    var username

--- to_do_app/lib/auth/authform.dart
+++ to_do_app/lib/auth/authform.dart
@@ -20,7 +20,7 @@ class _AuthFormState extends State<AuthForm> {
 
   var _email = '';
   var _password = '';
-
+  var _username = '';
   // we won't show the username if isLoginPage is true. It means that user is in login page. If isLoginPage is false, then user is in signup page. So we will show the username field
   bool isLoginPage = false;
 
